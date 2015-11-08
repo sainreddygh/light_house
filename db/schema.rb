4479f8ee1f7dec3cb1faa598c18c2eeb62d08da0
Sainath Reddy <nsainath.bits@gmail.com>
2015-11-07 19:48:29 -0800

    Removed the existing migration file and modified schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106075055) do
-
-  create_table "contacts", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.text     "comments"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20151108005330) do
 
 end
